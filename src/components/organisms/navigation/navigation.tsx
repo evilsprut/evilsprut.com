692df9dad898475f03e941c6293f62491d0f55a1
Aleksandr Bukhalo <aleksandr@bukhalo.com>
2020-03-01 14:15:47 +0300

    chore: some visual fixes and refactoring

---
+++ src/components/organisms/navigation/navigation.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'gatsby';
+
+const List = styled.ul`
+  margin: 0;
+  list-style: none;
+`;
+
+const ListItem = styled.li`
+  display: inline;
+  margin-right: 1.5rem;
+`;
+
+const NavigationLink = styled(Link)`
+  font-family: Montserrat, sans-serif;
+  font-size: 1rem;
+  font-weight: 900;
+  box-shadow: none;
+`;
+
+export const Navigation: React.FC = () => {
+  return (
+    <List>
+      <ListItem>
+        <NavigationLink to="/">Home</NavigationLink>
+      </ListItem>
+      <ListItem>
+        <NavigationLink to="/blog">Blog</NavigationLink>
+      </ListItem>
+    </List>
+  );
+};
